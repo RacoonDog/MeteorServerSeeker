3c6a7815c294a8d8dddc84343f9200f9bcca8ab2
RacoonDog <32882447+racoondog@users.noreply.github.com>
2024-11-18 01:39:40 -0500

    qol changes to server info screen

--- src/main/java/de/damcraft/serverseeker/gui/ServerInfoScreen.java
+++ src/main/java/de/damcraft/serverseeker/gui/ServerInfoScreen.java
@@ -9,6 +9,7 @@ import meteordevelopment.meteorclient.gui.WindowScreen;
 import meteordevelopment.meteorclient.gui.widgets.containers.WTable;
 import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
 import meteordevelopment.meteorclient.utils.network.Http;
+import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
@@ -34,28 +35,34 @@ public class ServerInfoScreen extends WindowScreen {
         add(theme.label("Fetching server info..."));
 
         ServerSeekerSystem system = ServerSeekerSystem.get();
-        system.invalidate();
 
         HostAndPort hap = HostAndPort.fromString(serverIp);
         ServerInfoRequest request = new ServerInfoRequest(system.apiKey, hap.getHost(), hap.getPort());
 
-        ServerInfoResponse response = Http.post("https://api.serverseeker.net/server_info")
-            .bodyJson(request)
-            .sendJson(ServerInfoResponse.class);
+        MeteorExecutor.execute(() -> {
+            ServerInfoResponse response = Http.post("https://api.serverseeker.net/server_info")
+                .bodyJson(request)
+                .sendJson(ServerInfoResponse.class);
 
-        clear();
+            ServerSeekerSystem.get().invalidate();
+            clear();
 
-        if (response == null) {
-            ServerSeekerSystem.get().networkIssue = true;
-            add(theme.label("Network error")).expandX();
-            return;
-        }
+            if (response == null) {
+                ServerSeekerSystem.get().networkIssue = true;
+                add(theme.label("Network error")).expandX();
+                return;
+            }
 
-        if (response.isError()) {
-            add(theme.label(response.error())).expandX();
-            return;
-        }
+            if (response.isError()) {
+                add(theme.label(response.error())).expandX();
+                return;
+            }
 
+            load(response, hap);
+        });
+    }
+
+    private void load(ServerInfoResponse response, HostAndPort hap) {
         Boolean cracked = response.cracked();
         String description = response.description();
         int onlinePlayers = response.onlinePlayers();
@@ -66,7 +73,6 @@ public class ServerInfoScreen extends WindowScreen {
         List<ServerInfoResponse.Player> players = response.players();
 
         WTable dataTable = add(theme.table()).widget();
-        WTable playersTable = add(theme.table()).expandX().widget();
 
         dataTable.add(theme.label("Cracked: "));
         dataTable.add(theme.label(cracked == null ? "Unknown" : cracked.toString()));
@@ -96,30 +102,35 @@ public class ServerInfoScreen extends WindowScreen {
         dataTable.add(theme.label("Version: "));
         dataTable.add(theme.label(version + " (" + protocol + ")"));
 
-        playersTable.add(theme.label(""));
-        playersTable.row();
-        playersTable.add(theme.label("Players:"));
-        playersTable.row();
+        if (!players.isEmpty()) {
+            WTable playersTable = add(theme.table()).expandX().widget();
 
-
-        playersTable.add(theme.label("Name ")).expandX();
-        playersTable.add(theme.label("Last seen ")).expandX();
-        playersTable.row();
-
-
-        playersTable.add(theme.horizontalSeparator()).expandX();
-        playersTable.row();
-
-        for (ServerInfoResponse.Player player : players) {
-            String name = player.name();
-            long playerLastSeen = player.lastSeen();
-            String lastSeenFormatted = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-                .format(Instant.ofEpochSecond(playerLastSeen).atZone(ZoneId.systemDefault()).toLocalDateTime());
-
-            playersTable.add(theme.label(name + " ")).expandX();
-            playersTable.add(theme.label(lastSeenFormatted + " ")).expandX();
+            playersTable.add(theme.label(""));
             playersTable.row();
+            playersTable.add(theme.label("Players:"));
+            playersTable.row();
+
+
+            playersTable.add(theme.label("Name ")).expandX();
+            playersTable.add(theme.label("Last seen ")).expandX();
+            playersTable.row();
+
+
+            playersTable.add(theme.horizontalSeparator()).expandX();
+            playersTable.row();
+
+            for (ServerInfoResponse.Player player : players) {
+                String name = player.name();
+                long playerLastSeen = player.lastSeen();
+                String lastSeenFormatted = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+                    .format(Instant.ofEpochSecond(playerLastSeen).atZone(ZoneId.systemDefault()).toLocalDateTime());
+
+                playersTable.add(theme.label(name + " ")).expandX();
+                playersTable.add(theme.label(lastSeenFormatted + " ")).expandX();
+                playersTable.row();
+            }
         }
+
         WButton joinServerButton = add(theme.button("Join this Server")).expandX().widget();
         joinServerButton.action = ()
             -> ConnectScreen.connect(new TitleScreen(), MinecraftClient.getInstance(), new ServerAddress(hap.getHost(), hap.getPort()), new ServerInfo("a", hap.toString(), ServerInfo.ServerType.OTHER), false, null);
